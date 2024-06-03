0282b1580a05cfb8182a9abe6bba22c5f1d11e5d
Jose E. Fuentes <efuenteswk@gmail.com>
2024-06-03 11:19:00 -0700

    Completed docker work.

--- src/main/java/com/emmfu/optimagrowth/license/model/License.java
+++ src/main/java/com/emmfu/optimagrowth/license/model/License.java
@@ -1,9 +1,10 @@
 package com.emmfu.optimagrowth.license.model;
 
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
-public class License {
+public class License extends RepresentationModel<License> {
     private Long Id;
     private String licenseId;
     private String description;
